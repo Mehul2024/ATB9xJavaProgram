e86e14267f0f6893ac1e7037a82dc7747bfa9dfa
Mehul <mehulpagi003@gmail.com>
2024-10-04 09:01:49 +0530

    lab 003

--- src/Oct/ex_03102024/Lab003.java
+++ src/Oct/ex_03102024/Lab003.java
@@ -3,9 +3,15 @@ package Oct.ex_03102024;
 public class Lab003 {
     public static void main(String[] args)
     {
-        System.out.println("Addition=5+2");
+        System.out.println("Simple Calculator");
+        System.out.println("Addition");
+        System.out.println("5+2=7");
         System.out.println("Subtraction=5-2");
+        System.out.println("5-2=3");
         System.out.println("Multiplication=5+2");
+        System.out.println("5*2=10");
         System.out.println("Division=5+2");
+        System.out.println("5/2=2.5");
     }
 }
+

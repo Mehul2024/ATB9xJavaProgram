b756104b650ca6d7153e06ff4d52e1c8d4d6a64e
Mehul2024 <165147506+Mehul2024@users.noreply.github.com>
2025-02-21 08:38:47 +0530

    Delete src directory

--- src/Oct/ex_02102024/Lab001.java
+++
@@ -1,9 +0,0 @@
-package Oct.ex_02102024;
-
-public class Lab001 {
-    public static void main(String [] args)
-    {
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-    }
-}
